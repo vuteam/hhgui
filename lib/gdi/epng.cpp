ce2f539d77a5eae6a84539b96a52dd6ceae6eaf2
vuteam <rakzami39@gmail.com>
2017-05-24 22:20:21 +0100

    Update epng.cpp

--- lib/gdi/epng.cpp
+++ lib/gdi/epng.cpp
@@ -74,12 +74,6 @@ int loadPNG(ePtr<gPixmap> &result, const char *filename)
 	
 	if (color_type == PNG_COLOR_TYPE_GRAY || color_type & PNG_COLOR_MASK_PALETTE)
 	{
-		if (bit_depth < 8)
-		{
-			png_set_packing(png_ptr);
-			bit_depth = 8;
-		}
-
 		result=new gPixmap(eSize(width, height), bit_depth);
 		gSurface *surface = result->surface;
 	
