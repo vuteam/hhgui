b59f7d8650d2807cd5640213130bc7000bbef03b
vuteam <rakzami39@gmail.com>
2017-05-25 12:30:13 +0100

    Update accel.h

--- lib/gdi/accel.h
+++ lib/gdi/accel.h
@@ -1,53 +1,31 @@
 #ifndef __lib_gdi_accel_h
 #define __lib_gdi_accel_h
 
-#include <base/elock.h>
-#include <list>
-
-struct gUnmanagedSurface;
+struct gSurface;
 class eRect;
 class ePoint;
 
 class gAccel
 {
 public:
-	static gAccel* getInstance() { return instance; }
+	static gAccel* getInstance();
 	gAccel();
-	~gAccel();
-
-	void releaseAccelMemorySpace();
+	~gAccel(); 
+	
 	void setAccelMemorySpace(void *addr, int phys_addr, int size);
-
-	bool hasAlphaBlendingSupport();
-	int blit(gUnmanagedSurface *dst, gUnmanagedSurface *src, const eRect &p, const eRect &area, int flags);
-	int fill(gUnmanagedSurface *dst, const eRect &area, unsigned long col);
-
-	int accelAlloc(gUnmanagedSurface* surface);
-	void accelFree(gUnmanagedSurface* surface);
-
-	void dumpDebug();
+	
+	int blit(gSurface *dst, const gSurface *src, const eRect &p, const eRect &area, int flags);
+	int fill(gSurface *dst, const eRect &area, unsigned long col);
+	
+	int accelAlloc(void *&addr, int &phys_addr, int size);
+	void accelFree(int phys_addr);
 private:
-	struct MemoryBlock {
-		gUnmanagedSurface *surface;
-		int index;
-		int size;
-
-		MemoryBlock(gUnmanagedSurface *o, int i, int s):
-			surface(o), index(i), size(s)
-		{}
-		MemoryBlock():
-			surface(0), index(0), size(0)
-		{}
-	};
-	typedef std::list<MemoryBlock> MemoryBlockList;
-
-	eSingleLock m_allocation_lock;
 	void *m_accel_addr;
 	int m_accel_phys_addr;
 	int m_accel_size; // in blocks
-	MemoryBlockList m_accel_allocation;
+	int *m_accel_allocation;
 	int m_bcm_accel_state;
-
+	
 	static gAccel *instance;
 };
 
